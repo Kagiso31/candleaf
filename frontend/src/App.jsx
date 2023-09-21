1588154f235d1afb6bff3b903f2264a6cbf859a4
Kagiso31 <kagisomokou@gmail.com>
2023-09-21 06:35:43 +0200

    Add navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/candleaf" element={<Home />} />
+      <Route path="/candleaf/cart" element={<Cart />} />
+      <Route path="/candleaf/*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
